bd0525f8730c4c27a7dd94794bab16ff6ea264e9
unknown <david.berrocaste@gmail.com>
2025-07-24 19:26:47 +0200

    intentando funcionar

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-
   plugins: [react()],
+  base: '/babapoly/', // ← ESTE NOMBRE DEBE COINCIDIR CON EL NOMBRE DEL REPO
 })
